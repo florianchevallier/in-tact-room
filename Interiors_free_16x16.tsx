7c6e3b81fb4c4ccb083b1ae76a3a49fec54776df
Florian Chevallier <florian.chevallier@in-tact.fr>
2021-02-04 17:37:56 +0100

    test

---
+++ Interiors_free_16x16.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Interiors_free_16x16" tilewidth="32" tileheight="32" tilecount="224" columns="8">
+ <image source="../../../Downloads/Modern tiles_Free/Interiors_free/16x16/Interiors_free_16x16.png" width="256" height="896"/>
+</tileset>
