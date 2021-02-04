0859b69cd7030f1f7d1abdb1d2f2dff3be498fee
florianchevallier <42930731+florianchevallier@users.noreply.github.com>
2021-02-04 16:38:24 +0000

    Deploying to gh-pages from master @ 7c6e3b81fb4c4ccb083b1ae76a3a49fec54776df 🚀

---
+++ test.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="test" tilewidth="16" tileheight="16" tilecount="896" columns="16">
+ <image source="../../../Downloads/Modern tiles_Free/Interiors_free/16x16/Interiors_free_16x16.png" width="256" height="896"/>
+</tileset>
